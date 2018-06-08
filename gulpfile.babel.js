93ca77d97ee0ff8cef5ac447d835f9a0d3b7bb5d
Stephan <stephan.github@blakeslee.uk>
2018-06-08 17:12:05 +0100

    Fix tests

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,79 +1,79 @@
-import gulp from "gulp";
-import eslint from "gulp-eslint";
-import babel from "gulp-babel";
-import rename from "gulp-rename";
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import babel from 'gulp-babel';
+import rename from 'gulp-rename';
 
-import browserify from "browserify";
-import buffer from "vinyl-buffer";
-import del from "del";
-import source from "vinyl-source-stream";
-import sourcemaps from "gulp-sourcemaps";
-import uglify from "gulp-uglify";
+import browserify from 'browserify';
+import buffer from 'vinyl-buffer';
+import del from 'del';
+import source from 'vinyl-source-stream';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
 
-gulp.task("lint", () => {
-  return gulp.src([ "*.js",
-    "src/**/*.js",
-    "test/**/*.js",
-    "!node_modules/**"
+gulp.task('lint', () => {
+  return gulp.src([ '*.js',
+    'src/**/*.js',
+    'test/**/*.js',
+    '!node_modules/**'
   ])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task("clean", () => {
-  return Promise.all([ del("dist/"), del("coverage/") ]);
+gulp.task('clean', () => {
+  return Promise.all([ del('dist/'), del('coverage/') ]);
 });
 
-gulp.task("build:bundled:min", function bundledMin () {
-  return buildBundle(bundledConfig, ".bundle.min.js", true);
+gulp.task('build:bundled:min', function bundledMin () {
+  return buildBundle(bundledConfig, '.bundle.min.js', true);
 });
 
-gulp.task("build:external:min", function () {
-  return buildBundle(externalConfig, ".min.js", true);
+gulp.task('build:external:min', function () {
+  return buildBundle(externalConfig, '.min.js', true);
 });
 
-gulp.task("build:bundled:debug", function () {
-  return buildBundle(bundledConfig, ".bundle.js", false);
+gulp.task('build:bundled:debug', function () {
+  return buildBundle(bundledConfig, '.bundle.js', false);
 });
 
-gulp.task("build:external:debug", function () {
-  return buildBundle(externalConfig, ".js", false);
+gulp.task('build:external:debug', function () {
+  return buildBundle(externalConfig, '.js', false);
 });
 
-gulp.task("build:components", function () {
-  return gulp.src("src/**")
+gulp.task('build:components', function () {
+  return gulp.src('src/**')
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist/components"))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/components'))
     ;
 });
 
 const bundledConfig = {
   debug: true,
-  entries: "src/starling.js",
-  standalone: "starling"
+  entries: 'src/starling.js',
+  standalone: 'starling'
 };
 const externalConfig = {
   debug: true,
-  entries: "src/starling.js",
-  standalone: "starling",
+  entries: 'src/starling.js',
+  standalone: 'starling',
   external: [
-    "axios",
-    "js-base64",
-    "es6-promise",
-    "debug",
-    "utf8"
+    'axios',
+    'js-base64',
+    'es6-promise',
+    'debug',
+    'utf8'
   ],
   bundleExternal: false
 };
 
 function buildBundle (options, extname, minify) {
   let stream = browserify(options)
-    .transform("babelify")
+    .transform('babelify')
     .bundle()
-    .pipe(source("starling.js"))
+    .pipe(source('starling.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }));
 
@@ -82,15 +82,15 @@ function buildBundle (options, extname, minify) {
   }
 
   return stream.pipe(rename({ extname }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 }
 
-gulp.task("build", gulp.series(
-  "build:bundled:min",
-  "build:external:min",
-  "build:bundled:debug",
-  "build:external:debug",
-  "build:components"
+gulp.task('build', gulp.series(
+  'build:bundled:min',
+  'build:external:min',
+  'build:bundled:debug',
+  'build:external:debug',
+  'build:components'
 ));
 
